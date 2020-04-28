6cc66881b6b7aa6a0fe3d46ea426a07e86a49e9d
Jeremie <jeremiegthr@gmail.com>
2020-04-28 11:48:16 +0200

    add navbar component

---
+++ layouts/navbar.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="style/navbar.css">
+
+<nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-bg">
+  <a class="navbar-brand" href="home.php">Camagru</a>
+  <div class="navbar-nav">
+    <?php if (isset($_SESSION["connected"])) { ?>
+      <a class="nav-item nav-link" href="#">Profil</a>
+      <a class="nav-item nav-link" href="#">Mon Compte</a>
+    <?php } else { ?>
+      <a class="nav-item nav-link" href="#">Connexion</a>
+      <a class="nav-item nav-link" href="#">Inscription</a>
+    <?php } ?>
+  </div>
+</nav>
