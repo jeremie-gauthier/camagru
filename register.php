98ed6186d36d71d01c8b55eb92af482d6bb2f6f7
Jeremie <jeremiegthr@gmail.com>
2020-04-28 14:33:44 +0200

    add validation client

--- register.php
+++ register.php
@@ -1,30 +1,65 @@
 <?php require_once "layouts/header.php" ?>
 <link rel="stylesheet" type="text/css" href="style/auth.css">
+<script type="text/javascript" src="scripts/formValidation.js"></script>
 
 <?php require_once "layouts/navbar.php" ?>
 
 <div class="container">
   <h1>Inscription</h1>
   <hr />
-  <form>
-    <div class="form-group">
+  <form name="register" method="POST" action="server/handleRegister.php" onsubmit="return submitForm()">
+    <div class="form-group no-margin">
       <label for="pseudo">Choisissez un pseudo</label>
-      <input type="text" class="form-control" name="pseudo" id="pseudo" maxlength="5" required>
+      <input type="text" class="form-control" name="pseudo" id="pseudo" maxlength="16" required>
+      <span class="text-danger feedback" id="pseudo-error"></span>
     </div>
-    <div class="form-group">
+    <div class="form-group no-margin">
       <label for="email">Adresse mail</label>
       <input type="email" class="form-control" name="email" id="email" required>
+      <span class="text-danger feedback" id="email-error"></span>
     </div>
-    <div class="form-group">
+    <div class="form-group no-margin">
       <label for="pwd">Mot de passe</label>
-      <input type="password" class="form-control" name="pwd" id="pwd" required>
+      <input type="password" class="form-control" name="pwd" id="pwd" minlength='8' required>
+      <span class="text-danger feedback" id="pwd-error"></span>
     </div>
     <div class="form-group">
       <label for="confirm-pwd">Confirmation mot de passe</label>
-      <input type="password" class="form-control" name="confirm-pwd" id="confirm-pwd" required>
+      <input type="password" class="form-control" name="confirm-pwd" id="confirm-pwd" minlength='8' required>
     </div>
-    <button type="submit" class="btn btn-primary">Inscription</button>
+    <button type="submit" value="Submit" class="btn btn-primary">Inscription</button>
   </form>
 </div>
 
 <?php require_once "layouts/footer.php" ?>
+
+<script>
+  const form = document.forms.register;
+  const [pseudoErr, emailErr, pwdErr] = mapElements([
+    "pseudo-error",
+    "email-error",
+    "pwd-error"
+  ]);
+
+  window.onload = () => {
+    form.pseudo.addEventListener('blur', () =>
+      checkPseudo(form.pseudo.value, pseudoErr)
+    );
+    form.email.addEventListener('blur', () =>
+      checkEmail(form.email.value, emailErr)
+    );
+    form.pwd.addEventListener('blur', () =>
+      checkPwd(form.pwd.value, form['confirm-pwd'].value, pwdErr)
+    );
+    form['confirm-pwd'].addEventListener('blur', () =>
+      checkPwd(form.pwd.value, form['confirm-pwd'].value, pwdErr)
+    );
+  }
+
+  const submitForm = () => {
+    checkPseudo(form.pseudo.value, pseudoErr);
+    checkEmail(form.email.value, emailErr);
+    checkPwd(form.pwd.value, form['confirm-pwd'].value, pwdErr);
+    return [pseudoErr, emailErr, pwdErr].every((elem) => elem.innerHTML === "");
+  }
+</script>
---
+++ scripts/formValidation.js
@@ -0,0 +1,54 @@
+const mapElements = (ids) => {
+	return ids.map((id) => document.getElementById(id));
+};
+
+const checkPseudo = (pseudo, err) => {
+	const pattern = /[^a-zA-Z\d\-]/;
+
+	if (pseudo === "") {
+		err.innerHTML = "Champ requis";
+	} else if (pseudo.length < 3) {
+		err.innerHTML = "Votre pseudo doit etre compose de 3 caracteres minimum";
+	} else if (pseudo.length > 16) {
+		err.innerHTML = "Votre pseudo doit etre compose de 16 caracteres maximum";
+	} else if (pseudo.match(pattern)) {
+		err.innerHTML =
+			"Votre pseudo ne doit contenir que des lettres, des chiffres ou un trait d'union";
+	} else {
+		err.innerHTML = "";
+	}
+};
+
+const checkEmail = (email, err) => {
+	const pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+	if (email === "") {
+		err.innerHTML = "Champ requis";
+	} else if (!email.match(pattern)) {
+		err.innerHTML = "Adresse mail non valide";
+	} else {
+		err.innerHTML = "";
+	}
+};
+
+const checkPwd = (pwd, confirmPwd, err) => {
+	const isUpper = () => pwd.match(/[A-Z]/);
+	const isLower = () => pwd.match(/[a-z]/);
+	const isDigit = () => pwd.match(/\d/);
+	const isSpecial = () =>
+		pwd.match(/[ !\"#\$%&'\(\)\*\+,-\.\/\\:;<=>\?@\[\]\^_`{\|}~]/);
+	const checkChar = () => isUpper() && isLower() && isDigit() && isSpecial();
+
+	if (pwd === "") {
+		err.innerHTML = "Champ requis";
+	} else if (pwd.length < 8) {
+		err.innerHTML = "Votre mot de passe doit contenir au moins 8 caracteres";
+	} else if (!checkChar()) {
+		err.innerHTML =
+			"Votre mot de passe doit contenir au moins 1 majuscule, 1 minuscule, 1 chiffre et 1 caractere special";
+	} else if (pwd !== confirmPwd) {
+		err.innerHTML = "Les mots de passe ne sont pas identiques";
+	} else {
+		err.innerHTML = "";
+	}
+};
