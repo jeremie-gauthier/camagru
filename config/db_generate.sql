1c896b63363b48564ccefb11d75651ede449dfce
Fviret <flo.viret@gmail.com>
2020-04-29 17:54:29 +0200

    le HTML du login

--- config/db_generate.sql
+++ config/db_generate.sql
@@ -1,6 +1,6 @@
 -- TESTS
 
-INSERT INTO users(pseudo, email, password) VALUES ("jergauth", "jergauth@student.42.fr", "abcd"), ("fviret", "fviret@student.42.fr", "abcd");
+INSERT INTO users(pseudo, email, password) VALUES ("jergauth", "jergauth@student.42.fr", "abcd"), ("flviret", "flviret@student.42.fr", "abcd");
 INSERT INTO pictures(diUsers, encoding, legend) VALUES (1, "AAAA", "Photo de vacances"), (2, "BBBB", NULL), (1, "AABB", NULL);
 INSERT INTO likes(diUsers, diPictures) VALUES (1, 1), (2, 1), (1, 2);
 INSERT INTO comments(diUsers, diPictures, comment) VALUES (2, 1, "Super photo"), (1, 1, "Merci !");
---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+  session_start();
+  require_once("server/utils/class/Session.php");
+?>
+<?php require_once "layouts/header.php" ?>
+<link rel="stylesheet" type="text/css" href="style/auth.css">
+<script type="text/javascript" src="scripts/formValidation.js"></script>
+
+<?php require_once "layouts/navbar.php" ?>
+
+<div class="container">
+  <h1>Connexion</h1>
+  <hr />
+  <form name="register" method="POST" action="server/handleRegister.php" onsubmit="return submitForm()">
+    <div class="form-group no-margin">
+      <label for="email">Adresse mail</label>
+      <input type="email" class="form-control" name="email" id="email" required>
+      <span class="text-danger feedback" id="email-error"></span>
+    </div>
+    <div class="form-group no-margin">
+      <label for="pwd">Mot de passe</label>
+      <input type="password" class="form-control" name="pwd" id="pwd" minlength='8' required>
+      <span class="text-danger feedback" id="pwd-error"></span>
+    </div>
+    <button type="submit" value="Submit" class="btn btn-primary">Connexion</button>
+  </form>
+  <span class="text-danger"><?php echo Session::get("register-err") ?></span>
+</div>
+
+<?php require_once "layouts/footer.php" ?>
+
+<script>
+  const form = document.forms.register;
+  const [pseudoErr, emailErr, pwdErr] = mapElements([
+    "email-error",
+    "pwd-error"
+  ]);
+
+  window.onload = () => {
+    form.email.addEventListener('blur', () =>
+      checkEmail(form.email.value, emailErr)
+    );
+    form['confirm-pwd'].addEventListener('blur', () =>
+      checkPwd(form.pwd.value, form['confirm-pwd'].value, pwdErr)
+    );
+  }
+
+  const submitForm = () => {
+    checkEmail(form.email.value, emailErr);
+    checkPwd(form.pwd.value, form['confirm-pwd'].value, pwdErr);
+    return [emailErr, pwdErr].every((elem) => elem.innerHTML === "");
+  }
+</script>
