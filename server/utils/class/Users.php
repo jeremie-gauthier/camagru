25557fb250966ab5ef63973e7db1bd96d9400621
Jeremie <jeremiegthr@gmail.com>
2020-04-29 20:07:15 +0200

    interface for user data management

---
+++ server/utils/class/Users.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "Database.php";
+
+class Users extends Database{
+  function __construct($DB_DSN, $DB_USER, $DB_PASSWORD) {
+    parent::__construct($DB_DSN, $DB_USER, $DB_PASSWORD);
+  }
+
+  function getByMail($email) {
+    try {
+      $query = "SELECT * FROM users WHERE email=:email";
+      $values = [":email" => $email];
+      $this->query($query, $values);
+      $user = $this->get_result();
+      return $user;
+    } catch (Exception $e) {
+      throw $e;
+    }
+  }
+
+  function create($pseudo, $email, $pwd) {
+    try {
+      // AJOUTER HASH ET VALIDATION MAIL PAR LA SUITE
+      $query = "INSERT INTO users (pseudo, email, password) VALUES (:pseudo, :email, :pwd)";
+      $values = [
+        ":pseudo" => $pseudo,
+        ":email" => $email,
+        ":pwd" => password_hash($pwd, PASSWORD_DEFAULT)
+      ];
+      $this->query($query, $values);
+    } catch (Exception $e) {
+      throw $e;
+    }
+  }
+}
+
+?>
