334aab7886457104403d33bb0c277a2b8fb88782
Jeremie <jeremiegthr@gmail.com>
2020-04-29 19:16:07 +0200

    redirections

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -3,12 +3,13 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-bg">
   <a class="navbar-brand" href="index.php">Camagru</a>
   <div class="navbar-nav">
-    <?php if (isset($_SESSION["connected"])) { ?>
+    <?php if (isset($_SESSION["pseudo"])) { ?>
       <a class="nav-item nav-link" href="#">Profil</a>
       <a class="nav-item nav-link" href="#">Mon Compte</a>
+      <a class="nav-item nav-link" href="logout.php">Deconnexion</a>
     <?php } else { ?>
-      <a class="nav-item nav-link" href="#">Connexion</a>
-      <a class="nav-item nav-link" href="#">Inscription</a>
+      <a class="nav-item nav-link" href="login.php">Connexion</a>
+      <a class="nav-item nav-link" href="register.php">Inscription</a>
     <?php } ?>
   </div>
 </nav>
