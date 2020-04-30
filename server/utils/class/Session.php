a89c6efbc0ff26eb7f2e39f8d3494d6f791e9b08
Jeremie <jeremiegthr@gmail.com>
2020-04-30 10:58:07 +0200

    add multiset

--- server/utils/class/Session.php
+++ server/utils/class/Session.php
@@ -1,28 +1,34 @@
 <?php
 
 class Session {
-  public function get($key) {
+  function get($key) {
     return $_SESSION[$key];
   }
 
-  public function set($key, $value) {
+  function set($key, $value) {
     $_SESSION[$key] = $value;
   }
 
-  public function update($key, $newValue) {
+  function multiset($array) {
+    foreach ($array as $key => $val) {
+      Session::set($key, $val);
+    }
+  }
+
+  function update($key, $newValue) {
     unset($_SESSION[$key]);
     $_SESSION[$key] = $newValue;
   }
 
-  public function append($key, $value) {
+  function append($key, $value) {
     $_SESSION[$key][] = $value;
   }
 
-  public function del($key) {
+  function del($key) {
     unset($_SESSION[$key]);
   }
 
-  public function exists($key) {
+  function exists($key) {
     return array_key_exists($key, $_SESSION);
   }
 }
