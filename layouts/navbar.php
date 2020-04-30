e74087227954163f6ee1de308ebd40795598d4cb
Jeremie <jeremiegthr@gmail.com>
2020-04-30 10:59:03 +0200

    use Session class to check session_vars

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,9 +1,10 @@
+<?php require_once "server/utils/class/Session.php" ?>
 <link rel="stylesheet" type="text/css" href="style/navbar.css">
 
 <nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-bg">
   <a class="navbar-brand" href="index.php">Camagru</a>
   <div class="navbar-nav">
-    <?php if (isset($_SESSION["pseudo"])) { ?>
+    <?php if (Session::exists("pseudo")) { ?>
       <a class="nav-item nav-link" href="#">Profil</a>
       <a class="nav-item nav-link" href="#">Mon Compte</a>
       <a class="nav-item nav-link" href="logout.php">Deconnexion</a>
