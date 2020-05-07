d420d9db14be94e3962f770099d83873dfbffc64
Jeremie <jeremiegthr@gmail.com>
2020-05-07 11:57:37 +0200

    state management

--- picture/scripts/camera.js
+++ picture/scripts/camera.js
@@ -6,6 +6,8 @@ const start = async () => {
 			video.srcObject = stream;
 			video.removeAttribute("hidden");
 			cam.innerHTML = "videocam_off";
+			snap.disabled = false;
+			uploadImg.disabled = true;
 		} else {
 			showToast(err.message);
 		}
@@ -17,11 +19,15 @@ const start = async () => {
 const stop = () => {
 	if (video.srcObject) {
 		video.setAttribute("hidden", "");
+		snap.disabled = true;
 		const tracks = video.srcObject.getTracks();
 		tracks.forEach((track) => track.stop());
 		state.recording = false;
 		cam.innerHTML = "videocam";
+		uploadImg.disabled = false;
 	}
 };
 
 const snapshot = () => {};
+
+const upload = () => {};
