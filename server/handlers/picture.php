45e5d2866ff4be1649cc14a02a901eca4034f7b8
Jeremie <jeremiegthr@gmail.com>
2020-05-08 23:13:57 +0200

    handle picture save

--- server/handlers/picture.php
+++ server/handlers/picture.php
@@ -1,3 +1,7 @@
 <?php
-  echo "Hello from server";
+  extract(array_map('htmlspecialchars', $_POST));
+  $data = explode('base64,', $picture)[1];
+  $img = str_replace(' ', '+', $data);
+  file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/assets/users/1.png", base64_decode($img));
+  echo "Image enregistree";
 ?>
