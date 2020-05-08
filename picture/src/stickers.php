169218ba3fc5a06efef298c8cc8a7be5d5ee509a
Jeremie <jeremiegthr@gmail.com>
2020-05-08 23:14:11 +0200

    change file architecture

--- picture/src/stickers.php
+++ picture/src/stickers.php
@@ -10,24 +10,24 @@
 <link rel="stylesheet" type="text/css" href="picture/styles/sticker.css">
 
 <div id="list-stickers" aria-labelledby="btn-stickers">
-  <div class="sticker" onclick="state.pic?.sticker.add('assets/Minion.png', 50, 50)">
-    <img class="img-sticker" src="assets/Minion.png" />
+  <div class="sticker" onclick="state.pic?.sticker.add('assets/stickers/Minion.png', 50, 50)">
+    <img class="img-sticker" src="assets/stickers/Minion.png" />
     <span class="text-sticker">Minion</span>
   </div>
-  <div class="sticker" onclick="state.pic?.sticker.add('assets/Soleil.png', 75, 75)">
-    <img class="img-sticker" src="assets/Soleil.png" />
+  <div class="sticker" onclick="state.pic?.sticker.add('assets/stickers/Soleil.png', 75, 75)">
+    <img class="img-sticker" src="assets/stickers/Soleil.png" />
     <span class="text-sticker">Soleil</span>
   </div>
-  <div class="sticker" onclick="state.pic?.sticker.add('assets/Cadre.png', 640, 480)">
-    <img class="img-sticker" src="assets/Cadre.png" />
+  <div class="sticker" onclick="state.pic?.sticker.add('assets/stickers/Cadre.png', 640, 480)">
+    <img class="img-sticker" src="assets/stickers/Cadre.png" />
     <span class="text-sticker">Cadre</span>
   </div>
-  <div class="sticker" onclick="state.pic?.sticker.add('assets/Geralt.png', 314, 250)">
-    <img class="img-sticker" src="assets/Geralt.png" />
+  <div class="sticker" onclick="state.pic?.sticker.add('assets/stickers/Geralt.png', 314, 250)">
+    <img class="img-sticker" src="assets/stickers/Geralt.png" />
     <span class="text-sticker">Geralt</span>
   </div>
-  <div class="sticker" onclick="state.pic?.sticker.add('assets/Ciri.png', 320, 400)">
-    <img class="img-sticker" src="assets/Ciri.png" />
+  <div class="sticker" onclick="state.pic?.sticker.add('assets/stickers/Ciri.png', 320, 400)">
+    <img class="img-sticker" src="assets/stickers/Ciri.png" />
     <span class="text-sticker">Ciri</span>
   </div>
 </div>
