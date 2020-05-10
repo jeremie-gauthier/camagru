e7378e13e7c2ef49f9dce55077202f93c0b548de
Jeremie <jeremiegthr@gmail.com>
2020-05-10 15:07:22 +0200

    work with json

--- server/handlers/picture.php
+++ server/handlers/picture.php
@@ -3,5 +3,10 @@
   $data = explode('base64,', $picture)[1];
   $img = str_replace(' ', '+', $data);
   file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/assets/users/1.png", base64_decode($img));
-  echo "Image enregistree";
+
+  $response = (object) [
+    "message" => "Image enregistree"
+  ];
+  $json = json_encode($response);
+  echo $json;
 ?>
