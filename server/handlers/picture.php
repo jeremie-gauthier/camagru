f8f795cf5e44caf7baa97ec50a6ba27fde4c1a13
Jeremie <jeremiegthr@gmail.com>
2020-05-10 16:18:05 +0200

    handle add img

--- server/handlers/picture.php
+++ server/handlers/picture.php
@@ -1,10 +1,32 @@
 <?php
-  extract(array_map('htmlspecialchars', $_POST));
-  $data = explode('base64,', $picture)[1];
-  $img = str_replace(' ', '+', $data);
-  file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/assets/users/1.png", base64_decode($img));
+  session_start();
+  require_once "../utils/class/Pictures.php";
+  require_once "../utils/class/Session.php";
+  require_once "../../config/database.php";
+
+  header("Content-Type: application/json; charset=UTF-8");
+
+  extract(
+    array_map(
+      'htmlspecialchars',
+      json_decode($_POST["data"], true)
+    )
+  );
+
+  $img = str_replace(
+    ' ',
+    '+',
+    explode('base64,', $picture)[1]
+  );
+  
+  $pic_cls = new Pictures($DB_DSN, $DB_USER, $DB_PASSWORD);
+  $imgId = $pic_cls->create(Session::get("userId"));
+
+  $baseURL = $_SERVER['DOCUMENT_ROOT'] . "/assets/users/";
+  file_put_contents($baseURL . $imgId . ".png", base64_decode($img));
 
   $response = (object) [
+    "imgId" => $imgId,
     "message" => "Image enregistree"
   ];
   $json = json_encode($response);
