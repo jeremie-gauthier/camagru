15c8a647691386602405214f526f20573e638e0c
Jeremie <jeremiegthr@gmail.com>
2020-05-10 15:09:09 +0200

    Create abstraction class to work with XHR Promised-based

---
+++ scripts/AsyncRequest.js
@@ -0,0 +1,89 @@
+class AsyncRequest {
+	static #XHRInstance = () => {
+		try {
+			return new XMLHttpRequest();
+		} catch (e) {
+			try {
+				return new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				return new ActiveXObject("Msxml2.XMLHTTP");
+			}
+		}
+	};
+
+	static get(url) {
+		return new Promise((resolve, reject) => {
+			const xhr = AsyncRequest.#XHRInstance();
+			xhr.open("GET", url, true);
+
+			xhr.onload = () => {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					resolve(response);
+				} catch (err) {
+					reject(err);
+				}
+			};
+
+			xhr.send(null);
+		});
+	}
+
+	static post(url, data) {
+		return new Promise((resolve, reject) => {
+			const xhr = AsyncRequest.#XHRInstance();
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+			xhr.onload = () => {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					resolve(response);
+				} catch (err) {
+					reject(err);
+				}
+			};
+
+			const json = JSON.stringify(data);
+			xhr.send(json);
+		});
+	}
+
+	static put(url, data) {
+		return new Promise((resolve, reject) => {
+			const xhr = AsyncRequest.#XHRInstance();
+			xhr.open("PUT", url, true);
+			xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+			xhr.onload = () => {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					resolve(response);
+				} catch (err) {
+					reject(err);
+				}
+			};
+
+			const json = JSON.stringify(data);
+			xhr.send(json);
+		});
+	}
+
+	static delete(url) {
+		return new Promise((resolve, reject) => {
+			const xhr = AsyncRequest.#XHRInstance();
+			xhr.open("DELETE", url, true);
+
+			xhr.onload = () => {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					resolve(response);
+				} catch (err) {
+					reject(err);
+				}
+			};
+
+			xhr.send(null);
+		});
+	}
+}
