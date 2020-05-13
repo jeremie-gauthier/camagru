dd04fd0dba41be04631f5660c71900c20490d41d
Jeremie <jeremiegthr@gmail.com>
2020-05-13 17:44:38 +0200

    send mail for reset pwd

--- account/settings/handler.php
+++ account/settings/handler.php
@@ -11,6 +11,9 @@
 
   if ($_SERVER["REQUEST_METHOD"] === "PUT") {
     try {
+      require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/validation/pseudo.php";
+      require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/validation/email.php";
+
       parse_str(file_get_contents('php://input'), $_PUT);
       extract(
         array_map(
@@ -18,15 +21,56 @@
           json_decode($_PUT["data"], true)
         )
       );
+
+      if (!checkPseudo($pseudo) || !checkEmail($email)) {
+        http_response_code(400);
+        echo "Formulaire invalide";
+        die(2);
+      }
+
       $userId = Session::get("userId");
 
       $user_cls = new Users($DB_DSN, $DB_USER, $DB_PASSWORD);
+
+      // ensure the mail can be used
+      $user = $user_cls->getByMail($email);
+      if ($user && $user[0]["idUsers"] != $userId) {
+        http_response_code(403);
+        echo "Adresse mail deja attribuee";
+        die(2);
+      }
+
       $user_cls->update($pseudo, $email, $userId);
       Session::update("pseudo", $pseudo);
       Session::update("email", $email);
       http_response_code(204);
     } catch (Exception $e) {
-      throw ($e);
+      http_response_code(500);
+      echo $e;
+      die(2);
     }  
+  }
+
+  else if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    try {
+      require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Mail.php";
+
+      $email = $_REQUEST['email'];
+      $userId = Session::get("userId");
+      $hash = md5(time());
+
+      $user_cls = new Users($DB_DSN, $DB_USER, $DB_PASSWORD);
+      $user_cls->bindHash($userId, $hash);
+      Mail::newPassword($email, $hash);
+    } catch (Exception $e) {
+      http_response_code(500);
+      echo $e;
+      die(2);
+    }
+  }
+
+  else {
+    http_response_code(401);
+    die(2);
   }
 ?>
