eddecea7612afbd9f8de86356937af9385bfa82f
Jeremie <jeremiegthr@gmail.com>
2020-05-13 13:05:53 +0200

    controlled form

--- account/settings/main.js
+++ account/settings/main.js
@@ -4,18 +4,46 @@ const emailErr = document.getElementById("email-error");
 const confirmBtn = document.getElementById("btn-confirm");
 const modifyBtn = document.getElementById("btn-modify");
 
-window.onload = () => {
-	form.pseudo.addEventListener("blur", () =>
-		checkPseudo(form.pseudo.value, pseudoErr)
-	);
-	form.email.addEventListener("blur", () =>
-		checkEmail(form.email.value, emailErr)
-	);
+// DEFINING STATES
+const props = {
+	origPseudo: form.pseudo.value,
+	origEmail: form.email.value,
+	pseudo: form.pseudo.value,
+	email: form.email.value,
+};
+
+const handlers = {
+	set: (obj, prop, value) => {
+		obj[prop] = value;
+		if (prop === "pseudo" || prop === "email") {
+			if (
+				state.pseudo === state.origPseudo &&
+				state.email === state.origEmail
+			) {
+				confirmBtn.disabled = true;
+			} else {
+				confirmBtn.disabled = false;
+			}
+		}
+	},
+};
+
+const state = new Proxy(props, handlers);
+
+// SET EVENTS
+form.pseudo.onkeyup = () => {
+	state.pseudo = form.pseudo.value;
+	checkPseudo(state.pseudo, pseudoErr);
+};
+
+form.email.onkeyup = () => {
+	state.email = form.email.value;
+	checkEmail(state.email, emailErr);
 };
 
 const submitForm = () => {
-	checkPseudo(form.pseudo.value, pseudoErr);
-	checkEmail(form.email.value, emailErr);
+	checkPseudo(state.pseudo, pseudoErr);
+	checkEmail(state.email, emailErr);
 	return [pseudoErr, emailErr].every((elem) => elem.innerHTML === "");
 };
 
@@ -27,8 +55,12 @@ const modifyBtnToggler = () => {
 		form.email.disabled = false;
 	} else {
 		modifyBtn.innerHTML = "Modifier mes informations";
+		form.pseudo.value = state.origPseudo;
+		form.email.value = state.origEmail;
 		confirmBtn.hidden = true;
 		form.pseudo.disabled = true;
 		form.email.disabled = true;
+		state.pseudo = state.origPseudo;
+		state.email = state.origEmail;
 	}
 };
