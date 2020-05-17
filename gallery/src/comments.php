53688175b82d6a95d1ba377027e36427ddf76e47
Jeremie <jeremiegthr@gmail.com>
2020-05-17 20:23:44 +0200

    fix import bug

--- gallery/src/comments.php
+++ gallery/src/comments.php
@@ -2,6 +2,8 @@
 
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Comments.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Users.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Pictures.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Session.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/config/database.php";
 
@@ -44,8 +46,9 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     $userId = Session::get("userId");
 
     $com_cls = new Comments($DB_DSN, $DB_USER, $DB_PASSWORD);
-    $com_cls->create($userId, $pictureId, $comment);
-
+    $id_new_comment = $com_cls->create($userId, $pictureId, $comment);
+    $new_comment = $com_cls->getById($id_new_comment);
+    
     $pic_cls = new Pictures($DB_DSN, $DB_USER, $DB_PASSWORD);
     $picture = $pic_cls->getById($pictureId);
     if (count($picture) != 1) {
@@ -63,7 +66,12 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     if ($user[0]["notifications"] == 1) {
       Mail::notifNewComment($user[0]["email"], Session::get("pseudo"));
     }
-    http_response_code(204);
+    http_response_code(201);
+    $response = (object) [
+      "comment" => $new_comment
+    ];
+    $json = json_encode($response);
+    echo $json;
   } catch (Exception $e) {
     http_response_code(500);
     echo $e->getMessage();
