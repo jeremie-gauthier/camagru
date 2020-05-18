d12fcc55f9d33e758f58629696335e7e67bddaaa
Jeremie <jeremiegthr@gmail.com>
2020-05-18 11:01:37 +0200

    public gallery

--- index.php
+++ index.php
@@ -1,14 +1,109 @@
 <?php 
   session_start();
-  require $_SERVER['DOCUMENT_ROOT'] . "/components/layouts/header.php";
+  require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/class/Session.php";
+  require_once $_SERVER['DOCUMENT_ROOT'] . "/components/layouts/header.php"
 ?>
-<link rel="stylesheet" type="text/css" href="/index.css">
+<link rel="stylesheet" type="text/css" href="/gallery/styles/gallery.css">
+<link rel="stylesheet" type="text/css" href="/gallery/styles/overlay.css">
 
-<?php require $_SERVER['DOCUMENT_ROOT'] . "/components/layouts/navbar.php" ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/layouts/navbar.php" ?>
 
-<div class="mid w-100 row"> 
-  <div class="main col-9"><p>oui</p></div> 
-  <div class="side col-2"><p>oui</p></div>
+<div class="container">
+  <h1>Accueil</h1>
+  <hr />
+
+  <a href="/picture" class="material-icons add-a-photo">add_a_photo</a>
+
+  <div id="no-picture" hidden>Vous n'avez partag&eacute; aucune photo.</div>
+  <div id="list-pictures">
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/components/card/index.php" ?>
+  </div>
+
+  <div id="overlay-legend-container" class="overlay-container" hidden>
+    <div class="overlay-content">
+      <h1 class="overlay-title">L&eacute;gende</h1>
+      <div id="overlay-legend-counter" class="overlay-counter">0/255</div>
+      <textarea
+        autofocus
+        id="overlay-legend-text"
+        class="overlay-text"
+        name="legend"
+        rows='10'
+        column='50'
+        maxlength="255"
+        placeholder="Ajoutez une l&eacute;gende..."
+      ></textarea>
+      <button
+        type="button"
+        id="overlay-legend-btn"
+        class="overlay-btn"
+        onclick="putLegend()"
+      >Enregistrer</button>
+    </div>
+  </div>
+
+  <div id="overlay-comment-container" class="overlay-container" hidden>
+    <div class="overlay-content">
+      <h1 class="overlay-title">Ajoutez un commentaire</h1>
+      <div id="overlay-comment-counter" class="overlay-counter">0/255</div>
+      <textarea
+        autofocus
+        id="overlay-comment-text"
+        class="overlay-text"
+        name="comment"
+        rows='10'
+        column='50'
+        maxlength="255"
+        placeholder="Ajoutez un commentaire..."
+      ></textarea>
+      <button
+        type="button"
+        id="overlay-comment-btn"
+        class="overlay-btn"
+        onclick="addComment()"
+      >Envoyer</button>
+    </div>
+  </div>
+
+  <?php require $_SERVER['DOCUMENT_ROOT'] . "/components/toast.php" ?>
 </div>
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . "/components/layouts/footer.php" ?>
+
+<script type="text/javascript" src="/utils/scripts/DOM.js"></script>
+<script type="text/javascript" src="/utils/scripts/throttle.js"></script>
+<script type="text/javascript" src="/utils/scripts/AsyncRequest.js"></script>
+<script type="text/javascript" src="/gallery/scripts/overlay.js"></script>
+<script type="text/javascript" src="/gallery/scripts/main.js"></script>
+
+<script type="text/javascript">
+  const listDiv = document.getElementById("list-pictures");
+  <?php if (Session::exists("pseudo") === true) { ?>
+    const userPseudo = "<?php echo Session::get("pseudo") ?>";
+  <?php } else { ?>
+    const userPseudo = null;
+  <?php } ?>
+
+  const loadPictures = async () => {
+    try {
+      
+      const hasMore = await fetchCards(listDiv, userPseudo);
+      if (hasMore === false) {
+        window.removeEventListener("scroll", loadPicturesOnScroll);
+      }
+    } catch (err) {
+      showToast("error", err.message ?? err);
+    }
+  }
+  const throttledLoading = throttle(loadPictures, 500);
+
+  const loadPicturesOnScroll = () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      throttledLoading();
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", throttledLoading);
+  window.addEventListener("scroll", loadPicturesOnScroll);
+
+</script>
