638d8e92c86356f624394eb7418db9f079878ea5
Ubuntu <ubuntu@ubuntu.home>
2020-06-12 09:19:43 +0000

    add description meta tag for SEO

--- components/layouts/header.php
+++ components/layouts/header.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
+  <meta name="description" content="Projet scolaire 42. Coder un mini Instagram-like.">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
